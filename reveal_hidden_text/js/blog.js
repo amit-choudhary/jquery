35c062373979c4cc60a879958a4a71e1c48b0b43
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-30 15:32:23 +0530

    Reveal Hidden Text Exercise

---
+++ reveal_hidden_text/js/blog.js
@@ -0,0 +1,22 @@
+function Blog(blogPostsList) {
+  this.blogPostsList = blogPostsList;
+}
+
+Blog.prototype.bindEvents = function() {
+  var _this = this;
+  _this.blogPostsList.click(function(event) {
+    $('#blog .excerpt').slideUp();
+    event.preventDefault();
+    _this.slideDownBlogPost(this);
+  } );
+}
+
+Blog.prototype.slideDownBlogPost = function(blogPost) {
+  $(blogPost).next('.excerpt').slideDown();
+}
+
+$(function() {
+var blogPostsList = $('#blog h3');
+var blogPost = new Blog(blogPostsList);
+blogPost.bindEvents();
+} );
