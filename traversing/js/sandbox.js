293edb2e9b08b2e592cfc0c597ae74141e75955a
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-30 11:46:43 +0530

    Fix indentation

--- traversing/js/sandbox.js
+++ traversing/js/sandbox.js
@@ -5,16 +5,23 @@ $(function() {
   } );
 
   // Select the search input text box, then traverse up to the form and add a class to the form.
-  $('label[for = q]').parents('form').addClass('newClass');
+  $('label[for = q]')
+    .parents('form')
+      .addClass('newClass');
 
   // Select the list item inside #myList that has a class of "current" and remove that class from it; add a class of "current" to the next list item.
   var listItem = $('#myList').find('.current');
   listItem.removeClass();
-  listItem.next().addClass('current');
+  listItem
+    .next()
+      .addClass('current');
 
   //Select the select element inside #specials; traverse your way to the submit button.
   var selectElement = $('#specials').find('select');
-  selectElement.closest('ul').find('input').css('background-color', 'green');
+  selectElement
+    .closest('ul')
+      .find('input')
+        .css('background-color', 'green');
 
   // Select the first list item in the #slideshow element; add the class "current" to it, and then add a class of "disabled" to its sibling elements.
   var firstListItem = $('#slideshow li:eq(0)');
