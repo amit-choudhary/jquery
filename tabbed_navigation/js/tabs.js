74b1d57c3e5f28b9e17c2995115f1d6f499d9922
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-07-31 11:51:40 +0530

    tabbed navigation exercise

---
+++ tabbed_navigation/js/tabs.js
@@ -0,0 +1,37 @@
+function TabbedNavigation(divModules) {
+  this.divModules = divModules;
+  this.newList = $('<ul></ul>');
+}
+
+TabbedNavigation.prototype.init = function() {
+  var _this = this;
+  _this.divModules.hide();
+  $(_this.divModules[0]).before(_this.newList);
+  _this.divModules.each(function() {
+    _this.newList.append($('<li></li>')
+                   .text($(this).find('h2')
+                                  .text()));
+  } );
+  _this.newList.click(function(event) {
+     $(this).find('li').removeClass('current');
+     $(event.target).addClass('current');
+    _this.showRelatedModule(event); 
+  } );
+}
+
+TabbedNavigation.prototype.showRelatedModule = function(listElement) {
+  this.divModules.hide();
+  var text = $(listElement.target).text();
+  this.divModules.each(function() {
+    if ($(this).find('h2').text() == text) {
+      $(this).show();
+      return;
+    }
+  } );
+}
+
+$(function() {
+  var divModules = $('.module');
+  var newNavigation = new TabbedNavigation(divModules);
+  newNavigation.init();
+} );
