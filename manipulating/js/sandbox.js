4802b0e25ac0f9e277de937b8957d777d3cb49ac
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-08-04 11:01:02 +0530

    optimise code and fix selectors

--- manipulating/js/sandbox.js
+++ manipulating/js/sandbox.js
@@ -1,25 +1,23 @@
 $(function() {
   // Add five new list items to the end of the unordered list #myList.
-  var list1 = $('<li></li>');
-  var list2 = $('<li></li>');
-  var list3 = $('<li></li>');
-  var list4 = $('<li></li>');
-  var list5 = $('<li></li>');
-  $('#myList').append(list1, list2, list3, list4, list5);
+  for (var i = 8; i < 13 ; i++) {
+    var newList = $('<li>List item ' + i + '</li>');
+    $('#myList').append(newList);
+  }
   // Remove the odd list items.
-  $('li:odd').remove();
+  $('#myList li:odd').remove();
   // Add another h2 and another paragraph to the last div.module.
   var newHeader = $('<h2></h2>').text('New Header');
-  $('div .module').prepend(newHeader);
   var newParagraph = $('<p></p>').text('New Paragraph');
-  $('div .module').append(newParagraph);
+  $('div .module').append(newHeader, newParagraph);
   // Add another option to the select element; give the option the value "Wednesday".
-  var newOption = $('<option></option>').val('Wednesday');
-  newOption.text('Wednesday');
-  $('select').append(newOption);
+  $('<option>', {
+    value: 'Wednesday',
+    text: 'Wednesday'
+  }).appendTo('select[name=day]')
   // Add a new div.module to the page after the last one; put a copy of one of the existing images inside of it.
-  var newDiv = $('<div></div>').addClass('module');
-  $('div .module').after(newDiv);
-  var newImage = $('<img></img>').attr('src', "images/fruit.jpg");
-  newDiv.append(newImage);
+  $('div.module').last().after($('<div>', {
+    html: $("img").eq(0).clone(),
+    class: 'module'
+  } ));
 } );
