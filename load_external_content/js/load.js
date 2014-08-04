76133b1f0705bad94e0026d172ec35f541d2af11
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-08-04 19:24:44 +0530

    Insert divisions after blog headings and load external data in to these divisions

---
+++ load_external_content/js/load.js
@@ -0,0 +1,36 @@
+function LoadExternalContent(blogHeadings) {
+  this.blogHeadings = blogHeadings;
+  this.newDiv = '';
+}
+
+LoadExternalContent.prototype.init = function() {
+  var _this = this;
+  _this.insertDiv();
+  _this.blogHeadings.click(function(event) {
+    event.preventDefault();
+    _this.getContent($(this));
+  });
+};
+
+LoadExternalContent.prototype.insertDiv = function() {
+  var _this = this;
+  _this.blogHeadings.each(function() {
+      _this.newDiv = $('<div></div>').insertAfter($(this));
+      $(this).data('newDiv', _this.newDiv);
+    });
+};
+
+LoadExternalContent.prototype.getContent = function(currentHeading) {
+  var anchor = currentHeading.find('a'),
+      newDiv = currentHeading.data('newDiv'),
+      href = anchor.attr('href'),
+      url = href.split('#')[0],
+      id = href.split('#')[1];
+  newDiv.load(url + ' #' + id);
+};
+
+$(function() {
+  var blogHeadings = $('#blog h3'),
+      loadObject = new LoadExternalContent(blogHeadings);
+  loadObject.init();
+} );
