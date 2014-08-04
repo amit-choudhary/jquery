c5e6505f94e211966286f527ec0f870dfc5dd7de
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-08-04 12:18:23 +0530

    Optimise code, fix selectors and use chaining

--- traversing/js/sandbox.js
+++ traversing/js/sandbox.js
@@ -10,24 +10,17 @@ $(function() {
       .addClass('newClass');
 
   // Select the list item inside #myList that has a class of "current" and remove that class from it; add a class of "current" to the next list item.
-  var listItem = $('#myList').find('.current');
-  listItem.removeClass();
-  listItem
+  $('#myList .current')
+    .removeClass('current')
     .next()
       .addClass('current');
 
   //Select the select element inside #specials; traverse your way to the submit button.
-  var selectElement = $('#specials').find('select');
-  selectElement
-    .closest('ul')
-      .find('input')
-        .css('background-color', 'green');
+  $('#specials select').parents('ul').find('.input_submit').css('background-color', 'green');
 
   // Select the first list item in the #slideshow element; add the class "current" to it, and then add a class of "disabled" to its sibling elements.
-  var firstListItem = $('#slideshow li:eq(0)');
-  firstListItem.addClass('current');
-  var siblingElements = firstListItem.siblings();
-  siblingElements.each(function() {
-    $(this).addClass('disabled');
-  } );
+  $('#slideshow li:eq(0)')
+    .addClass('current')
+    .siblings()
+      .addClass('disabled');
 } );
