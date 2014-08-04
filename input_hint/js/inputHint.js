e50be5a554bf6db6a6066d46edf06917ef3a0273
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-08-04 11:47:43 +0530

    Use chaining, refactor code, fix functioning, optimise code

--- input_hint/js/inputHint.js
+++ input_hint/js/inputHint.js
@@ -1,36 +1,53 @@
-function searchElement() {
-  this.searchInput = $(':text[name="q"]');
-  this.labelElement = $('label[for="q"]');
-  labelText = this.labelElement.text();
+function CreateInputHint(searchInputElement, labelElement, labelText) {
+  this.searchInput = searchInputElement;
+  this.labelElement = labelElement;
+  this.hintText = labelText;
 }
 
-searchElement.prototype.init = function() {
-  this.searchInput.val(labelText);
-  this.searchInput.addClass('hint');
+CreateInputHint.prototype.init = function() {
+  this.searchInput
+    .val(this.hintText)
+    .addClass('hint');
+  this.labelElement.remove();
   this.bindFocusEvent();
   this.bindBlurEvent();
 }
 
-searchElement.prototype.bindFocusEvent = function() {
-  this.searchInput.focus(function() {
-    $(this).val('');
-    $(this).removeClass('hint');
+CreateInputHint.prototype.bindFocusEvent = function() {
+  var _this = this;
+  _this.searchInput.focus(function() {
+    _this.performOnFocus(this);
   } );
 }
 
-searchElement.prototype.bindBlurEvent = function() {
-  this.searchInput.blur(function() {
-    if($(this).val() == '' || $(this).val() == null){
-      $(this).val(labelText);
-      $(this).addClass('hint'); 
-    }
-    else {
-      return;
-    }
+CreateInputHint.prototype.performOnFocus = function(searchElement) {
+  $(searchElement)
+    .val('')
+    .removeClass('hint'); 
+}
+
+CreateInputHint.prototype.bindBlurEvent = function() {
+  var _this = this;
+  _this.searchInput.blur(function() {
+    _this.performOnBlur(this); 
   } );
+}
+
+CreateInputHint.prototype.performOnBlur = function(searchElement) {
+  if (searchElement.value.trim() == '' || this.val() == null) {
+    $(searchElement)
+      .val(this.hintText)
+      .addClass('hint'); 
+  }
+  else {
+    return;
+  } 
 }
 
 $(function() {
-  var searchInput = new searchElement();
-  searchInput.init();
+  var searchInputElement = $(':text[name="q"]'),
+      labelElement = $('label[for="q"]'),
+      labelText = labelElement.text(),
+      inputHint = new CreateInputHint(searchInputElement, labelElement, labelText);
+  inputHint.init();
 } );
