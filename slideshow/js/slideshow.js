ee1b45d0877eb76fd09364a3ab80ff35c0dc9b8c
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-08-05 17:08:28 +0530

    Refactor code, start index with 0 and remove use of detach

--- slideshow/js/slideshow.js
+++ slideshow/js/slideshow.js
@@ -1,12 +1,14 @@
 function Slideshow(slideshowElement, listElements) {
   this.slideshowElement = slideshowElement;
   this.listElements = listElements;
+  this.numberOfImages = listElements.length;
+  this.currentIndex = 0;
 }
 
 Slideshow.prototype.init = function() {
   this.navigationArea = $('<div></div>');
   this.slideshowElement.append(this.navigationArea);
-  $('body').prepend(this.slideshowElement.detach());
+  $('body').prepend(this.slideshowElement);
   this.listElements.hide();
   this.startSlideshow()
 }
@@ -19,17 +21,20 @@ Slideshow.prototype.fadeInAndOut = function(index) {
 }
 
 Slideshow.prototype.startSlideshow = function() {
-  var _this = this,
-      index = 1;
-  _this.numberOfImages = _this.listElements.length;
-  _this.fadeInAndOut(0);
+  var _this = this;
+  _this.fadeInAndOut(_this.currentIndex);
+  _this.currentIndex++;
   setInterval(function() {
-    _this.fadeInAndOut(index);
-    index++;
-    if (index == _this.numberOfImages) {
-      index = 0;
-    }
+    _this.startfading();
   }, 4000);
+}
+
+Slideshow.prototype.startfading = function() {
+  this.fadeInAndOut(this.currentIndex);
+  this.currentIndex++;
+  if (this.currentIndex == this.numberOfImages) {
+    this.currentIndex = 0;
+  }
 }
 
 $(function() {
