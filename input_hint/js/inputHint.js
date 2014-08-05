4e41ebe5c6f2e40c0332be0d79c5511c21237740
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-08-05 15:00:44 +0530

    Fix functioning on focus, optimise code, refactor code

--- input_hint/js/inputHint.js
+++ input_hint/js/inputHint.js
@@ -5,42 +5,49 @@ function CreateInputHint(searchInputElement, labelElement, labelText) {
 }
 
 CreateInputHint.prototype.init = function() {
-  this.searchInput
-    .val(this.hintText)
-    .addClass('hint');
-  this.labelElement.remove();
+  this.addHint();
+  this.removeLabelElement();
   this.bindFocusEvent();
   this.bindBlurEvent();
 }
 
+CreateInputHint.prototype.addHint = function() {
+  this.searchInput
+    .val(this.hintText)
+    .addClass('hint');
+}
+
+CreateInputHint.prototype.removeLabelElement = function() {
+  this.labelElement.remove();
+}
+
 CreateInputHint.prototype.bindFocusEvent = function() {
   var _this = this;
-  _this.searchInput.focus(function() {
-    _this.performOnFocus(this);
+  _this.searchInput.focus(function(event) {
+    _this.performOnFocus();
   } );
 }
 
-CreateInputHint.prototype.performOnFocus = function(searchElement) {
-  $(searchElement)
+CreateInputHint.prototype.performOnFocus = function() {
+  if(this.searchInput.val() == this.hintText) {
+  this.searchInput
     .val('')
     .removeClass('hint'); 
+  }
 }
 
 CreateInputHint.prototype.bindBlurEvent = function() {
   var _this = this;
   _this.searchInput.blur(function() {
-    _this.performOnBlur(this); 
+    _this.performOnBlur(); 
   } );
 }
 
-CreateInputHint.prototype.performOnBlur = function(searchElement) {
-  if (searchElement.value.trim() == '' || this.val() == null) {
-    $(searchElement)
+CreateInputHint.prototype.performOnBlur = function() {
+  if (this.searchInput.val().trim() == '' || this.searchInput.val() == null) {
+    this.searchInput
       .val(this.hintText)
       .addClass('hint'); 
-  }
-  else {
-    return;
   } 
 }
 
