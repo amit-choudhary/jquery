80976e9d19d6667251946d19109ae4ed4a82e518
Amit Choudhary <amitchoudhary1008@gmail.com>
2014-08-05 17:46:32 +0530

    Use color key inside html, optimize the way of sending variables to class, refactor code, optimise code

--- load_content_json/js/specials.js
+++ load_content_json/js/specials.js
@@ -1,11 +1,15 @@
-function LoadJson(specialsDiv, specialsForm, selectElement) {
+function LoadJson(specialsDiv, elements) {
 this.specialsDiv = specialsDiv;
-this.specialsForm = specialsForm;
-this.selectElement = selectElement;
+this.specialsForm = elements.specialsForm;
+this.selectElement = elements.selectElement;
 this.targetDiv = $('<div></div>');
 }
 
 LoadJson.prototype.init = function() {
+  this.insertDiv();
+}
+
+LoadJson.prototype.insertDiv = function() {
   this.specialsForm.after(this.targetDiv);
 }
 
@@ -13,25 +17,41 @@ LoadJson.prototype.loadJsonContent = function() {
   var _this = this;
   this.selectElement.change(function(event) {
     var jsonTarget = event.target.value;
-    $.ajax( { 
-        dataType : 'json',
-        url: './data/specials.json',
-        cache: true,
-        success:function(result, status, xhr) {
-          _this.targetDiv.html('<h3>' + result[jsonTarget].title + '</h3>' +
-            '<h4>' + result[jsonTarget].text + '</h4>' +
-            '<img src=.' + result[jsonTarget].image + '></img>');
-          _this.specialsForm.find(':submit').remove();
-        }
-      } );
+    _this.loadcontentUsingAjax(jsonTarget);
   } );
+}
+
+LoadJson.prototype.loadcontentUsingAjax = function(jsonTarget) {
+  var _this = this;
+  $.ajax( { 
+    dataType : 'json',
+    url: './data/specials.json',
+    cache: true,
+    success:function(result, status, xhr) {
+      _this.writeHtml(jsonTarget, result);
+    }
+  } ); 
+}
+
+LoadJson.prototype.writeHtml = function(jsonTarget, result) {
+  this.targetDiv.html('<font color=' + result[jsonTarget].color + 
+    '<h3>' + result[jsonTarget].title + '</h3>' +
+    '<h4>' + result[jsonTarget].text + '</h4>' +
+    '<img src=.' + result[jsonTarget].image + '></img></font>');
+  this.removeSubmitButton();
+}
+
+LoadJson.prototype.removeSubmitButton = function() {
+  this.specialsForm.find(':submit').remove();
 }
 
 $(function() {
   var specialsDiv = $('#specials');
-  var specialsForm = specialsDiv.find('form');
-  var selectElement = specialsDiv.find('select');
-  var loadJson = new LoadJson(specialsDiv, specialsForm, selectElement);
+  var elements = {
+    specialsForm: specialsDiv.find('form'),
+    selectElement: specialsDiv.find('select')
+  }
+  var loadJson = new LoadJson(specialsDiv, elements);
   loadJson.init();
   loadJson.loadJsonContent();
 } );
